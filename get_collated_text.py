6b34b620040fa2b476e8ffc9c324751d65426d52
kaldan007 <kaldantenzin@gmail.com>
2022-04-14 17:56:43 +0530

    text with namId prepared

--- get_collated_text.py
+++ get_collated_text.py
@@ -1,7 +1,10 @@
+import csv
+import logging
 import yaml
 from pathlib import Path
 
-from pedurma.preview import get_reconstructed_text
+# logging.basicConfig(filename="cross_vol_list.log", level=logging.INFO, filemode="w")
+logging.basicConfig(filename="missing_text.log", level=logging.INFO, filemode="w")
 
 def from_yaml(yml_path):
     return yaml.safe_load(yml_path.read_text(encoding="utf-8"))
@@ -9,11 +12,11 @@ def from_yaml(yml_path):
 def to_yaml(dict_):
     return yaml.safe_dump(dict_, sort_keys=False, allow_unicode=True,)
 
-def _mkdir(path):
-    if path.is_dir():
-        return path
-    path.mkdir(exist_ok=True, parents=True)
-    return path
+# def _mkdir(path):
+#     if path.is_dir():
+#         return path
+#     path.mkdir(exist_ok=True, parents=True)
+#     return path
 
 
 def get_collated_text(text_id):
@@ -28,20 +31,47 @@ def get_collated_text(text_id):
     return collated_text
 
 
-def get_pandita_text(nalanda_pandita_work_info):
+def get_pandita_text(nalanda_pandita_work_info, nazom_text_list):
     text_id = nalanda_pandita_work_info[0]
     text_code = nalanda_pandita_work_info[1]
-    text_title = nalanda_pandita_work_info[3]
+    text_title = nalanda_pandita_work_info[2]
     if text_id:
+        # if is_cross_vol_text(text_id):
+        #     logging.info(f"{text_id}")
         collated_text = get_collated_text(text_id)
-        for vol_id, text in collated_text.items():
-            Path(f'./nalanda_works/{text_code}_{vol_id}.txt').write_text(text, encoding='utf-8')
-        print(f'{text_id} completed..')
+        if collated_text:
+            for vol_id, text in collated_text.items():
+                Path(f'./nalanda_works/{text_code}{text_id}.txt').write_text(text, encoding='utf-8')
+        else:
+            if text_id not in nazom_text_list:
+                logging.info(f'{text_id} missing..')
+
+    
+def is_cross_vol_text(text_id):
+    collated_text_paths = list(Path('./collated_text').iterdir())
+    collated_text_paths.sort()
+    for collated_text_path in collated_text_paths:
+        collated_text_id = collated_text_path.stem[:-5]
+        if f"{text_id}x" == collated_text_id or f"{text_id}y" == collated_text_id:
+            return True
+    return False
+
+def get_text_list(list_path):
+    text_list = list_path.read_text(encoding='utf-8').splitlines()
+    text_list = list(set(text_list))
+    text_list.sort()
+    return text_list
 
 if __name__ == "__main__":
-    nalanda_pandita_works = Path('./nalanda_karchak.txt').read_text(encoding='utf-8')
-    nalanda_pandita_works = nalanda_pandita_works.splitlines()[1:]
-    for nalanda_pandita_work in nalanda_pandita_works:
-        nalanda_pandita_work_info = nalanda_pandita_work.split(',')
-        get_pandita_text(nalanda_pandita_work_info)
+    nazom_text_list = get_text_list(Path('./nazom_text_list.txt'))
+    karchak_file = open("nalanda_text_karchak.csv")
+
+    karchak_csvreader = csv.reader(karchak_file)
+
+    karchak = {}
+    rows = []
+    for row in karchak_csvreader:
+        rows.append(row)
     
+    for text_info in rows[1:]:
+        get_pandita_text(text_info, nazom_text_list)
